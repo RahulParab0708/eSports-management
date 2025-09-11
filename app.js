a8afdbcf22342f9175d5f5547b3ef896a195d6a7
rahul <rahulparab3005@gmail.com>
2025-09-11 16:20:22 +0530

    Changed port configuration

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ db.connect((error) => {
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 
-app.listen(5001, () => {
+const PORT = process.env.PORT || 5001;
+app.get("/",(req,res) =>{
+    res.send("Hello from render")
+})
+
+app.listen(PORT, () => {
     console.log("Server started on port 5001");
 })
