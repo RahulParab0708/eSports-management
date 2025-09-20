b50a50d31deb8848977510b075bfee9784660f49
RahulParab0708 <rahulparab3005@gmail.com>
2025-09-20 19:28:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
-const express = require("express");
-const path = require("path")
-const mysql = require("mysql");
 const dotenv = require("dotenv");
-
-dotenv.config({ path: './.env'});
+dotenv.config();
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
- db = mysql.createConnection({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-         port : process.env.DB_PORT,
-
+db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-const publicDirectory = path.join(__dirname,'./public');
+const publicDirectory = path.join(__dirname, "./public");
 app.use(express.static(publicDirectory));
 
-
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
 
-app.set('view engine','hbs');
+app.set("view engine", "hbs");
 
 db.connect((error) => {
-  if(error){
-    console.log(error)
-  }else{
-    console.log("MYSQL Connected....")
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("MYSQL Connected....");
   }
-})
+});
 
 //Define Routes
-app.use('/', require('./routes/pages'));
-app.use('/auth', require('./routes/auth'));
+app.use("/", require("./routes/pages"));
+app.use("/auth", require("./routes/auth"));
 
-const port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Server started on http://localhost:${PORT}`);
 });
+
