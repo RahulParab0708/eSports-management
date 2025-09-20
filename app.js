c6ac6123f886bbd7ff8c29d83bb8c072e129b5ea
RahulParab0708 <rahulparab3005@gmail.com>
2025-09-20 19:46:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,35 +2,19 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const path = require("path");
+const db = require("./controllers/db"); // import the db instance
 
 const app = express();
-
-db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
 
 const publicDirectory = path.join(__dirname, "./public");
 app.use(express.static(publicDirectory));
 
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.json());
 
 app.set("view engine", "hbs");
 
-db.connect((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("MYSQL Connected....");
-  }
-});
-
-//Define Routes
+// Routes
 app.use("/", require("./routes/pages"));
 app.use("/auth", require("./routes/auth"));
 
@@ -38,4 +22,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server started on http://localhost:${PORT}`);
 });
-
